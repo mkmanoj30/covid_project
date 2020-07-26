33a01628da6f42444be3d8197e973592cb856171
manoj <mkmanoj.kumar@outlook.com>
2020-07-26 21:05:00 +0530

    Updates

---
+++ precache-manifest.2d47c59eafead49c749cb5e2fac1eb32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a5a79cbcbfc3b9d66f4715995523e1",
+    "url": "/covid_project/index.html"
+  },
+  {
+    "revision": "b4cc6cd934f0b0ae96fe",
+    "url": "/covid_project/static/css/main.4e6d3b7b.chunk.css"
+  },
+  {
+    "revision": "1cd9fe2a9bfd3bd1f526",
+    "url": "/covid_project/static/js/2.5da2c103.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_project/static/js/2.5da2c103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4cc6cd934f0b0ae96fe",
+    "url": "/covid_project/static/js/main.a12e0da6.chunk.js"
+  },
+  {
+    "revision": "6e8d9b9acbffa0a95f3f",
+    "url": "/covid_project/static/js/runtime-main.2a98584e.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/covid_project/static/media/covid19_logo.6ce9074d.png"
+  }
+]);
